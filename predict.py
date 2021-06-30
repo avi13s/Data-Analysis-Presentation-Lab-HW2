f8f819046ded5b0cb3c8f10650f9a73b82dd0eb7
avi13s <59297204+avi13s@users.noreply.github.com>
2021-06-30 19:50:29 +0300

    Update predict.py
    download pickle from google drive

--- predict.py
+++ predict.py
@@ -14,7 +14,42 @@ parser.add_argument('input_folder', type=str, help='Input folder path, containin
 args = parser.parse_args()
 
 DIR_Test = args.input_folder
+
+# Preparing to download model pickle from google drive
+#taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+file_id = '1-d3fzELRE4X8gHP3J9aawcfKwllTKZ0g'
 DIR_pkl = 'checkpoint_model2_34000.pth'
+download_file_from_google_drive(file_id, DIR_pkl)
 
 
 def check_validity(xmin,xmax,ymin,ymax):
